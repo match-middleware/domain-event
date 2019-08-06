3feaf55012f980f6e30577aa79128c95726eb20f
zhangchao <jaguar_zc@sina.com>
2019-08-06 09:38:48 +0800

    1.提交第一个可用版本

---
+++ src/main/java/com/github/middleware/utils/JDKTypeUtils.java
@@ -0,0 +1,58 @@
+package com.github.middleware.utils;
+
+/**
+ * @Author zhangchao
+ * @Date 2019/8/5 17:49
+ * @Version v1.0
+ */
+public class JDKTypeUtils {
+
+
+    /**
+     * 判断object是否为基本类型
+     * @param object
+     * @return
+     */
+    public static boolean isBaseType(Object object) {
+        Class className = object.getClass();
+        if (className.equals(java.lang.Integer.class) ||
+                className.equals(java.lang.Byte.class) ||
+                className.equals(java.lang.Long.class) ||
+                className.equals(java.lang.Double.class) ||
+                className.equals(java.lang.Float.class) ||
+                className.equals(java.lang.Character.class) ||
+                className.equals(java.lang.Short.class) ||
+                className.equals(java.lang.Boolean.class)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否为基本类型的默认值
+     * @param object
+     * @return
+     */
+    public static boolean isBaseDefaultValue(Object object) {
+        Class className = object.getClass();
+        String strClassName = "" + className;
+        if(className.equals(java.lang.Integer.class)) {
+            return (int)object == 0;
+        } else if(className.equals(java.lang.Byte.class)) {
+            return (byte)object == 0;
+        } else if(className.equals(java.lang.Long.class)) {
+            return (long)object == 0L;
+        } else if(className.equals(java.lang.Double.class)) {
+            return (double)object == 0.0d;
+        } else if(className.equals(java.lang.Float.class)) {
+            return (float)object == 0.0f;
+        } else if(className.equals(java.lang.Character.class)) {
+            return (char)object == '\u0000';
+        } else if(className.equals(java.lang.Short.class)) {
+            return (short)object == 0;
+        } else if(className.equals(java.lang.Boolean.class)) {
+            return (boolean)object == false;
+        }
+        return false;
+    }
+}
